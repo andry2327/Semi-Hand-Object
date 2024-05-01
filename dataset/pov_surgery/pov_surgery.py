581307625a533834b4e16ce79f4f1edc3ec3b469
andry2327 <andry2327@gmail.com>
2024-05-01 17:57:35 +0200

    folder restructure

--- dataset/pov_surgery/pov_surgery.py
+++ dataset/pov_surgery/pov_surgery.py
@@ -15,10 +15,10 @@ import sys
 sys.path.append('/content/Semi-Hand-Object/')
 sys.path.append('/content/Semi-Hand-Object/dataset')
 sys.path.append('/content/Semi-Hand-Object/dataset/pov_surgery')
-from pov_surgery.preprocessing import load_img, get_bbox, process_bbox, generate_patch_image, augmentation
-from pov_surgery.transforms import world2cam, cam2pixel, pixel2cam, rigid_align, transform_joint_to_other_db
-from pov_surgery.vis import vis_keypoints, vis_mesh, save_obj, vis_keypoints_with_skeleton
-from pov_surgery.mano import MANO
+from preprocessing import load_img, get_bbox, process_bbox, generate_patch_image, augmentation
+from transforms import world2cam, cam2pixel, pixel2cam, rigid_align, transform_joint_to_other_db
+from vis import vis_keypoints, vis_mesh, save_obj, vis_keypoints_with_skeleton
+from mano import MANO
 import pickle
 import dataset.dataset_util as dataset_util
 
